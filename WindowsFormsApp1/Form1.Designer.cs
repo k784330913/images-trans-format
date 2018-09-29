a3ac9013908819e8137e18164ac166086628fa25
kkkk <784330913@qq.com>
2018-09-29 15:58:31 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "请指定输出目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(291, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(466, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "请选择图片文件夹";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(291, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(146, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "转换成Gif";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "转换成png";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(359, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "转换成bmp";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(466, 154);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "转换成icon";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 203);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Trans_img_format";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string fileName = null;
+        public string[] files = null;    //获得的所有文件路径
+        public string[] pattern = { "*.gif", "*.jpg", "*.png", "*.bmp" };//定义搜寻的图片格式
+        public string outfolderpath = null; //输出文件夹路径
+        public static Bitmap ReadImageFile(string path) //返回图片流
+        {
+            FileStream fs = File.OpenRead(path); //OpenRead
+            int filelength = 0;
+            filelength = (int)fs.Length; //获得文件长度 
+            Byte[] image = new Byte[filelength]; //建立一个字节数组 
+            Bitmap bit=null;
+            try
+            {
+                fs.Read(image, 0, filelength); //按字节流读取 
+                System.Drawing.Image result = System.Drawing.Image.FromStream(fs);
+                fs.Close();
+                bit = new Bitmap(result);
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+            return bit;
+        }
+
+        private void button3_Click(object sender, EventArgs e) //选择图片文件夹按钮
+        {
+            string folderpath = null;
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择图片所在文件夹";
+            dialog.ShowNewFolderButton = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                folderpath = dialog.SelectedPath;
+                if (string.IsNullOrEmpty(folderpath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                textBox2.Text = Convert.ToString(folderpath);
+                textBox1.Text = Convert.ToString(folderpath);
+                files = GetImages(folderpath, pattern);
+                outfolderpath = folderpath;
+                //foreach (string file in files)
+                //{
+                //    MessageBox.Show(file);
+                //}
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e) //选择输出文件夹按钮
+        {
+            string folderpath = null;
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择图片输出目录";
+            dialog.ShowNewFolderButton = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                folderpath = dialog.SelectedPath;
+                if (string.IsNullOrEmpty(folderpath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                textBox1.Text = Convert.ToString(folderpath);
+                outfolderpath = folderpath;
+            }
+        }
+        private void trans(string format) //转换并存储函数
+        {
+            if ( files==null || files.Length==0)
+            {
+                MessageBox.Show("请选择含有图片的文件夹！");
+                return;
+            }
+            foreach (string file in files)
+            {
+                Bitmap bit = ReadImageFile(file);
+                string nfile = Path.ChangeExtension(file, "." + format);
+                string nnfile = Path.GetFileName(nfile); //文件名和后缀
+                Random rnd = new Random();
+                string ran = rnd.Next(1, 100).ToString();
+                string nnnfile = outfolderpath + "\\" + ran + nnfile; //文件绝对路径
+                try
+                {
+                    switch (format)
+                        {
+                            case "gif":
+                                bit.Save(nnnfile, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                            case "png":
+                                bit.Save(nnnfile, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                            case "bmp":
+                                bit.Save(nnnfile, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                            case "ico":
+                                bit.Save(nnnfile, System.Drawing.Imaging.ImageFormat.Icon);
+                            break;
+                            default:
+                                MessageBox.Show("There was an error." +
+                                "Check the " + file);
+                            break;
+                        }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("There was an error." +
+                        "Check the "+ file);
+                    return;
+                }
+            }
+            MessageBox.Show("转换"+ format +"完成！");
+        }
+        private string[] GetImages(string dirPath, params string[] searchPatterns) //https://blog.csdn.net/wulex/article/details/78796507 getfiles()获取多种后缀的文件路径
+        {
+            if (searchPatterns.Length <= 0)
+            {
+                return null;
+            }
+            else
+            {
+                DirectoryInfo di = new DirectoryInfo(dirPath);
+                FileInfo[][] fis = new FileInfo[searchPatterns.Length][];
+                int count = 0;
+                for (int i = 0; i < searchPatterns.Length; i++)
+                {
+                    FileInfo[] fileInfos = di.GetFiles(searchPatterns[i]);
+                    fis[i] = fileInfos;
+                    count += fileInfos.Length;
+                }
+                string[] files = new string[count];
+                int n = 0;
+                for (int i = 0; i <= fis.GetUpperBound(0); i++)
+                {
+                    for (int j = 0; j < fis[i].Length; j++)
+                    {
+                        string temp = fis[i][j].FullName;
+                        files[n] = temp;
+                        n++;
+                    }
+                }
+                return files;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e) //转换成Gif
+        {
+            trans("gif");
+        }
+        private void button2_Click(object sender, EventArgs e) //转换成png
+        {
+            trans("png");
+        }
+
+        private void button5_Click(object sender, EventArgs e) //转换成bmp
+        {
+            trans("bmp");
+        }
+
+        private void button6_Click(object sender, EventArgs e) //转换成icon
+        {
+            trans("ico");
+        }
+    }
+}
